41a3d266f3f1f81420650eabba3edb60e351e300
amit saharana <amitsaharana@gmail.com>
2020-12-22 15:45:22 +0530

    typescript errors

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
 declare module 'sr-equation-editor' {
   import * as React from 'react';
-  export function addStyles(): void
-
+  
   export interface KeypadProps extends Omit<React.HTMLAttributes<HTMLSpanElement>, 'onChange'> {
     onChange?: (val: string) => void
     latex?: string
--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ import Keypad from "./components/keypad";
 import "mathquill/build/mathquill";
 import style from "mathquill/build/mathquill.css";
 
-export function addStyles() {
-  if (document.getElementById('react-mathquill-styles') == null) {
-    const styleTag = document.createElement('style')
-    styleTag.setAttribute('id', 'react-mathquill-styles')
-    styleTag.innerHTML = style[0][1]
-
-    const head = document.getElementsByTagName('head')[0]
-    head.appendChild(styleTag)
-  }
-}
 
 class App extends React.PureComponent {
   constructor(props) {
