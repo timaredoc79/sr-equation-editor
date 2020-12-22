943a8f58451f697b4ab496c332e420acfc9231f2
amit saharana <amitsaharana@gmail.com>
2020-12-22 12:47:40 +0530

    styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import * as ReactDOM from "react-dom";
 import Keypad from "./components/keypad";
 
 import "mathquill/build/mathquill";
-import "mathquill/build/mathquill.css";
+import style from "mathquill/build/mathquill.css";
+
+export function addStyles() {
+  if (document.getElementById('react-mathquill-styles') == null) {
+    const styleTag = document.createElement('style')
+    styleTag.setAttribute('id', 'react-mathquill-styles')
+    styleTag.innerHTML = style[0][1]
+
+    const head = document.getElementsByTagName('head')[0]
+    head.appendChild(styleTag)
+  }
+}
 
 class App extends React.PureComponent {
   constructor(props) {
@@ -27,3 +38,5 @@ class App extends React.PureComponent {
   }
 }
 ReactDOM.render(<App />, document.getElementById("root"));
+
+export default Keypad;
